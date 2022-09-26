653ae7501d858c84e674cc40be67872b2ea77552
feedmob <caok1231@gmail.com>
2022-09-26 23:39:41 +0800

    update install script

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/zsh
 git clone https://github.com/caok/mydotfiles.git ~/.dotfiles
-ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/tumx.conf ~/.tmux.conf
+mv ~/.dotfiles/vim ~/.vim
+ln -s ~/.vim/vimrc ~/.vimrc
 
-mkdir ~/.dotfiles/vim/autoload
-mkdir ~/.dotfiles/vim/plugged
-curl -fLo ~/.dotfiles/vim/autoload/plug.vim --create-dirs \
+mkdir ~/.vim/autoload
+mkdir ~/.vim/plugged
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
