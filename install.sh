438d203bbed554bf6f1b326c1cf870c9782c49af
feedmob <caok1231@gmail.com>
2022-09-26 23:23:58 +0800

    update install

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-git clone https://github.com/caok/mydotfiles ~/.dotfiles
+git clone https://github.com/caok/mydotfiles.git ~/.dotfiles
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/tumx.conf ~/.tmux.conf
